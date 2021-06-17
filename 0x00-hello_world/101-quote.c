52145b484ae27b17806df0fcb489b63ecdef71b3
edielam <edguard738@gmail.com>
2021-06-17 19:07:47 +0000

    final corrections

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print without using puts or printf *
+ * Return: return value of 1
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
