532b6f3424ac958fa141594d87936aeede4c6e90
edielam <edguard738@gmail.com>
2021-06-17 18:12:12 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - another simple print statement
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
