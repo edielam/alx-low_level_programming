c0c54804518c6e6a8a6220fd009e878337ce379e
edielam <edguard738@gmail.com>
2021-06-17 18:25:18 +0000

    sizes in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the sizes of data types
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+return (0);
+}
