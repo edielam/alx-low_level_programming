ff12dadae3331ac53c4a1d5541acf3d7947f9675
edielam <edguard738@gmail.com>
2021-06-20 12:52:48 +0000

    alphabets in reverse with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print all alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{int c;
+int d;
+for (c = 'z'; c >= 'a'; c--)
+{putchar(c); }
+putchar(10);
+return (0);
+}
