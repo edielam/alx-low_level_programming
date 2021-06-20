4664bb2ba490f398cf34f299bf1e16e4b4f05038
edielam <edguard738@gmail.com>
2021-06-20 16:55:25 +0000

    file 101

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print out combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+if (a != b && a < b)
+{putchar(a);
+putchar(b);
+if (a != 56)
+{putchar(44);
+putchar(32);
+}}
+
+}
+}
+putchar(10);
+return (0);
+}
