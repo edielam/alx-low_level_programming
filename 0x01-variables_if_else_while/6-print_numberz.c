56af02026932cf6ebde4b96ba382e2b19e52dcff
edielam <edguard738@gmail.com>
2021-06-20 12:48:15 +0000

    print numbers using getchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print numbers with putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{ putchar(a); }
+putchar(10);
+return (0);
+}
