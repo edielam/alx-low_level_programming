4801f7aa5586c26c9cef1d3f3902cd372afc13f8
edielam <edguard738@gmail.com>
2021-06-20 11:58:22 +0000

    more loops

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print with exceptions
+ * Return: 0
+ */
+int main(void)
+{int c;
+	for (c = 'a'; c < 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			{putchar(c);
+}}
+	putchar(10);
+	return (0);
+}
