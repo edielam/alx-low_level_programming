a90eec088498ca3c3d1db7b2cf1fdeb02b363ef5
edielam <edguard738@gmail.com>
2021-06-20 11:07:52 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+int ch = 'a';
+while (ch <= 'z')
+{putchar(ch);
+putchar(10);
+ch++; }
+return (0);
+}
