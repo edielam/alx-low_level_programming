cfa9711523f786840843ba601b95ba6d7b4ff00f
edielam <edguard738@gmail.com>
2021-06-20 15:17:47 +0000

    hex putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print out hex
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 48; a <= 57; a++)
+{putchar(a); }
+for (b = 65; b <= 70; b++)
+{putchar(b); }
+putchar(10);
+return (0);
+}
