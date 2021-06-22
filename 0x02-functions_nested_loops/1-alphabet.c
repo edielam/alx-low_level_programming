3c11e3530a88a12af899827c9a2b7a65df911eb9
edielam <edguard738@gmail.com>
2021-06-22 21:22:58 +0000

    functions in c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * @void: 0;
+ */
+
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{ _putchar(i); }
+_putchar('\n');
+}
