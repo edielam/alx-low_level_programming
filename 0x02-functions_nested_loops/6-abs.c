c03d9582f44d9846695e6e275448c08627ce3e12
edielam <edguard738@gmail.com>
2021-06-23 11:07:01 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _abs - prints absolute value
+ * @n : parameter
+ * Return: always return 0
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{n = -n;
+return (n); }
+else
+return (n);
+}
