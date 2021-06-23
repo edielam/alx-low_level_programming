1fd8d0a11c4e24fede03f1157855f1972b5d9400
edielam <edguard738@gmail.com>
2021-06-23 17:31:04 +0000

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,3 +5,17 @@
  */
 
 void times_table(void);
+{
+int i, j;
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58; j++)
+{
+_putchar(i * j);
+_putchar(44);
+_putchar(32);
+if (j == 9)
+{_putchar('\n'); }
+}
+}
+}
