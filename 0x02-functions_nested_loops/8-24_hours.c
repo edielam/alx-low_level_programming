7823ebcd6cc5594e29e34039e624916cfb30ae80
edielam <edguard738@gmail.com>
2021-06-23 16:12:13 +0000

    timer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - printing the minutes
+ * Return : 0 
+ */
+
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (i = 48; i <= 50; i++)
+{
+for (j = 48; i <= 57; j++)
+{
+for (k = 48; j <= 53; k++)
+{
+for (l = 48; l <= 57; l++)
+{
+if (i == 50 && j == 52)
+break;
+_putchar(i);
+_putchar(j);
+_putchar(58);
+_putchar(k);
+_putchar(l);
+_putchar(32);
+}
+}
+}
+}
+}
