44c2b427865d601633ed105d1e31d5d838637280
edielam <edguard738@gmail.com>
2021-06-30 22:39:14 +0000

    more pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_strlen - length of string
+ *@s: input param
+ *Return: always zero
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+i++;
+{
+return (i);
+}
+}
