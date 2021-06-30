b58b18051b91ef2180432ad0ee02750b93267ae9
edielam <edguard738@gmail.com>
2021-06-30 22:56:31 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts - print string
+ * @str: input param
+ * Return: always zero
+ */
+
+void _puts(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+}
