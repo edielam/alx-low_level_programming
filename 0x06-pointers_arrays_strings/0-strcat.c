e3c5e746d84e03c70932f9bc9d8823968babfafd
edielam <edguard738@gmail.com>
2021-07-01 15:13:25 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * _strcat - concat strings
  * @dest: input 1
@@ -6,26 +7,26 @@
  * Return: always 0
  */
 
-char _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-    //Pointer should not null pointer
-    if((dest == NULL) && (src == NULL))
-        return NULL;
-    //Create copy of s1
-    char *start = dest;
-    //Find the end of the destination string
-    while(*start != '\0')
-    {
-        start++;
-    }
-    //Now append the source string characters
-    //until not get null character of s2
-    while(*src != '\0')
-    {
-        *start++ = *src++;
-    }
-    //Append null character in the last
-    *start = '\0';
-    return dest;
+/* Pointer should not null pointer */
+if ((dest == NULL) && (src == NULL))
+return (NULL);
+/* Create copy of s1 */
+char *start = dest;
+/* Find the end of the destination string */
+while (*start != '\0')
+{
+start++;
+}
+/* Now append the source string characters */
+/* until not get null character of src */
+while (*src != '\0')
+{
+*start++ = *src++;
+}
+/* Append null character in the last */
+*start = '\0';
+return (dest);
 }
 
