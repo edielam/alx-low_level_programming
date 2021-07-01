d88a38679f5cb1cffff07f586bc5892f12539984
edielam <edguard738@gmail.com>
2021-07-01 13:58:29 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcat - concat strings
+ * @dest: input 1
+ * @src: input 2
+ * Return: always 0
+ */
+
+char _strcat(char *dest, char *src)
+{
+int j, k;
+i = 0;
+j = strlen(dest)
+k = strlen(src)
+for (i = 0, i < j; i++)
+{_putchar(dest[i]); }
+for (i = 0, i <= k; i++)
+{ _putchar(src[0]); }
+return (0);
+}
