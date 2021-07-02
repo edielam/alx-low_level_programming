825a8c06a55a3649f8804dc7da0a6527c6c6e863
edielam <edguard738@gmail.com>
2021-07-02 01:35:37 +0000

    str compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: always zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1)
+{
+if (*s1 != *S2)
+{
+break;
+}
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
