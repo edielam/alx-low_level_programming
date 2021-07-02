11c1211eac10bb4fb9003101e0a39e4b5aa2ee14
edielam <edguard738@gmail.com>
2021-07-02 02:56:24 +0000

    corrections

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
  * reverse_array - reverses an array
+ * print_array - print
  * @a: input array
  * @n: number of elements in array
  * Return: always 0
@@ -19,4 +20,20 @@ for (int i = 0; i < n; i++)
 {
 a[i] = aux[i];
 }
+}
+
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i++;
+}
+printf("\n");
 }
