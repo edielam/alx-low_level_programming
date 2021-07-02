5e60ac68c527e91d280fd9a1c5ef8fe2443ae9d9
edielam <edguard738@gmail.com>
2021-07-02 16:59:30 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * *rot13 - function that encodes a string using rot13.
+ * @s: pointer pointed to the variable s , of type character
+ * Return: pointer of type char.
+ */
+char *rot13(char *str)
+{
+	int i;
+	char *p = str;
+
+	char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*str != '\0')
+	{
+		i = 0;
+		while (i < 53)
+		{
+			if (*str == original[i])
+			{
+				*str = code[i];
+				break;
+			}
+			i++;
+		}
+		str++;
+	}
+	return (p);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,42 @@
 #include "holberton.h"
+#define MAX 100
 /**
  * *cap_string - function
  * @str: Pointer
  * Return: poniter return to function.
  */
 
-char *cap_string(char *str)
-{
-	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
-			'?', '"', '(', ')', '{', '}'};
-	int i, j;
-
-	for (i = 0; str[i] != '\0'; i++)
+char *cap_string(char *s)
+{int i;
+	s[MAX]={0};
+	//capitalize first character of words
+	for(i=0; s[i]!='\0'; i++)
 	{
-		if (str[0] >= 97 && str[0] <= 122)
+		//check first character is lowercase alphabet
+		if(i==0)
 		{
-			str[0] = str[0] - 32;
+			if((s[i]>='a' && s[i]<='z'))
+				s[i]=s[i]-32; //subtract 32 to make it capital
+			continue; //continue to the loop
 		}
-		for (j = 0; j < 14; j++)
-			if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
-				str[i] = str[i] - 32;
+		if(s[i]==' ')//check space
+		{
+			//if space is found, check next character
+			++i;
+			//check next character is lowercase alphabet
+			if(s[i]>='a' && s[i]<='z')
+			{
+				s[i]=s[i]-32; //subtract 32 to make it capital
+				continue; //continue to the loop
+			}
+		}
+		else
+		{
+			//all other uppercase characters should be in lowercase
+			if(s[i]>='A' && s[i]<='Z')
+				s[i]=s[i]+32; //subtract 32 to make it small/lowercase
+		}
 	}
-	return (str);
+	
+	return 0;
 }
-
