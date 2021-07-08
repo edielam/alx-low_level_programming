802cf57af882135c11d35f7dbc0817266bd32e0c
edielam <edguard738@gmail.com>
2021-07-08 15:05:18 +0000

    printstring by recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,14 @@
  */
 
 void _puts_recursion(char *s)
-{int i;
-  i = 0;
-  if (s[i] != '\0')
-    { _putchar(s[i]);
-      i++; }
-  return _puts_recursion(char *s);
-    if (s[i] == '\0');
-      return ('\n');
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s+1);
+}
+else
+{
+_putchar('\n');
+}
 }
