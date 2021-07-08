ded2a992543d5a22e19a20388222280be5527ef3
edielam <edguard738@gmail.com>
2021-07-08 15:39:28 +0000

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - print string reverse
+ * @s : input string
+ * Return: always zero
+ */
+
+void _print_rev_recursion(char *s)
+{
+int i;
+i = 0;
+if (*(s+i) == '\0')
+{
+return;
+}
+i++;
+_print_rev_recursion(s + i);
+_putchar(s[i]);
+}
