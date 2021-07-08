5d7dc72823956e2f2f1c658e9cd910eef2451514
edielam <edguard738@gmail.com>
2021-07-08 17:45:32 +0000

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,11 @@ void _print_rev_recursion(char *s)
 {
 int i;
 i = 0;
-if (*(s+i) == '\0')
+if (*(s + i) == '\0')
 {
 return;
 }
 i++;
 _print_rev_recursion(s + i);
-_putchar(s[i]);
+ _putchar(*s);
 }
