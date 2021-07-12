2b45a1a6a5d3fc89567c0f03cbad2e089670dd32
edielam <edguard738@gmail.com>
2021-07-12 16:52:22 +0000

    print no of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of args
+ * @argc: arg count
+ * @argv: string of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d/n", argc);
+return (0);
+}
