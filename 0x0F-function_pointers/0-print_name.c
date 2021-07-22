c89e099015bf8bd845b7a8334a13fb7c23df164c
edielam <edguard738@gmail.com>
2021-07-22 00:48:02 +0000

    functpoint

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointer.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL %% f != NULL)
+f(name);
+}
