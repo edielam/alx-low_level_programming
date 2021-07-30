76aeda232f400769800fecc749c6858783ada2a5
edielam <edguard738@gmail.com>
2021-07-30 23:45:21 +0000

    file 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_LISTS
-#define HOLBERTON_LISTS
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stddef.h>
 
@@ -19,9 +19,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
-int _putchar(char c);
+int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-#endif 
+#endif /* _LISTS_H_ */
